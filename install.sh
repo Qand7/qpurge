851697df41d58f18246be759c64ef3cfd95116f2
Qandy <145124536+Qand7@users.noreply.github.com>
2025-09-10 22:16:49 +0200

    Update install.sh
    Added qtest and structured commands

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 GOALDIR="/usr/bin"
 
 sudo cp qpurge/qpurge "$GOALDIR"/qpurge
+sudo cp qpurge/qtest "$GOALDIR"/qtest
+
 sudo chmod +x "$GOALDIR"/qpurge
+sudo chmod +x "$GOALDIR"/qtest
 
 echo "----------------------------"
-echo "Finished, qpurge is ready."
+echo "Finished, qpurge and qtest are ready."
 echo
 echo "Type \"qpurge -h\" for help."

f3d9e979c4ee97cc7c4c0553fbb80b7dd5646519
Qandy <145124536+Qand7@users.noreply.github.com>
2025-09-10 22:58:41 +0200

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+sudo chmod -x /usr/bin/qpurge
+sudo chmod -x /usr/bin/qtest
+sudo rm -rf /usr/bin/qtest
+sudo rm -rf /usr/bin/qpurge
+sudo rm -rf ~/qpurge-qtest
+
+echo "-------------------"
+echo "Deleted all Files."
